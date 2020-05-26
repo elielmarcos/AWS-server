97e56610c47e98f0254c01dff5c7b70e66857b6b
Eliel Romancini <elielmarcos@hotmail.com>
2020-05-25 23:14:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const app = express();
 
-let 
-
 app.get('/', (request,response)=>{
     return response.json({message: 'Server is UP!', hour: Math.round(+new Date()/1000)});
 });
